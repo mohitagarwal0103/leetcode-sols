185f11dcc0fdcecc9cdb7e358592d9d4bb03f910
mohitagarwal0103 <115355632+mohitagarwal0103@users.noreply.github.com>
2023-10-14 12:39:45 +0530

    Added solution - LeetHub

---
+++ Account Merge - GFG/account-merge.cpp
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+
+
+class Solution{
+  public:
+    vector<vector<string>> accountsMerge(vector<vector<string>> &arr) {
+        map<string ,int>mp;
+        int n=arr.size();
+        DisjointSet ds(n);
+        for (int i=0;i<n;i++)
+        {
+            for (int j=1;j<arr[i].size();j++)
+            {
+                if (!mp.count(arr[i][j]))
+                {
+                    mp[arr[i][j]]=i;
+                }
+                else
+                {
+                    ds.unionByRank(i,mp[arr[i][j]]);
+                }
+            }
+        }
+        vector<string>merged[n];
+        for (auto it:mp)
+        {
+            string a=it.first;
+            int b=it.second;
+            int upr=ds.findUPar(b);
+            merged[upr].push_back(a);
+        }
+        vector<vector<string>>ans;
+        for (int i=0;i<n;i++)
+        {
+            if (merged[i].size()==0)
+            continue;
+            sort(merged[i].begin(),merged[i].end());
+            vector<string>tmp;
+            tmp.push_back(arr[i][0]);
+            for (auto it:merged[i])
+            {
+                tmp.push_back(it);
+            }
+            ans.push_back(tmp);
+        }
+        return ans;
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    vector<vector<string>> accounts;
+
+    for (int i = 0; i < n; i++)
+    {
+      vector<string> temp;
+      int x;
+      cin >> x;
+
+      for (int j = 0; j < x; j++)
+      {
+        string s1;
+        cin >> s1;
+        temp.push_back(s1);
+      }
+      accounts.push_back(temp);
+    }
+
+    Solution obj;
+    vector<vector<string>> res = obj.accountsMerge(accounts);
+    sort(res.begin(), res.end());
+    cout << "[";
+    for (int i = 0; i < res.size(); ++i)
+    {
+      cout << "[";
+      for (int j = 0; j < res[i].size(); j++)
+      {
+        if (j != res[i].size() - 1)
+          cout << res[i][j] << ","
+               << " ";
+        else
+          cout << res[i][j];
+      }
+      if (i != res.size() - 1)
+        cout << "], " << endl;
+      else
+        cout << "]";
+    }
+    cout << "]"
+         << endl;
+  }
+}
+// } Driver Code Ends
