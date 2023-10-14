a9eb540c34cb1532903c4bd690b0ae6f59f5b438
mohitagarwal0103 <115355632+mohitagarwal0103@users.noreply.github.com>
2023-10-14 11:51:35 +0530

    Added solution - LeetHub

---
+++ Connecting the graph - GFG/connecting-the-graph.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+
+
+class Solution {
+  public:
+    int Solve(int n, vector<vector<int>>& ed) {
+        DisjointSet ds(n);
+        int extra=0;
+        for (int i=0;i<ed.size();i++)
+        {
+            int a=ed[i][0];
+            int b=ed[i][1];
+            if (ds.findUPar(a)==ds.findUPar(b))
+            {
+                extra++;
+            }
+            else
+            {
+                ds.unionByRank(a,b);
+            }
+        }
+        int compo=0;
+        for (int i=0;i<n;i++)
+        {
+            if (ds.findUPar(i)==i)
+            {
+                compo++;
+            }
+        }
+        if (extra>=compo-1)
+        {
+            return compo-1;
+        }
+        else 
+        {
+            return -1;
+        }
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 2; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution Obj;
+        cout << Obj.Solve(n, adj) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
