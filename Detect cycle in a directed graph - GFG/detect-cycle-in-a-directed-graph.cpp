fc8c96e2ac194281c046ef06622528b0908aae69
mohitagarwal0103 <115355632+mohitagarwal0103@users.noreply.github.com>
2023-11-29 18:33:07 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -3,44 +3,78 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool solve(int i ,vector<int>&vis,vector<int>&path,vector<int>adj[])
-    {
-        vis[i]=1;
-        path[i]=1;
-        for (auto it:adj[i])
-        {
-            if (!vis[it])
-            {
-                bool a=solve(it,vis,path,adj);
-                if (a)
-                return true;
-            }
-            else if (path[it])
-            {
-                return true;
-            }
-        }
-        path[i]=0;
-        return false;
-    }
+    // bool solve(int i ,vector<int>&vis,vector<int>&path,vector<int>adj[])
+    // {
+    //     vis[i]=1;
+    //     path[i]=1;
+    //     for (auto it:adj[i])
+    //     {
+    //         if (!vis[it])
+    //         {
+    //             bool a=solve(it,vis,path,adj);
+    //             if (a)
+    //             return true;
+    //         }
+    //         else if (path[it])
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //     path[i]=0;
+    //     return false;
+    // }
     bool isCyclic(int n, vector<int> adj[]) {
-        vector<int>vis(n,0);
-        vector<int>path(n,0);
+        // vector<int>vis(n,0);
+        // vector<int>path(n,0);
+        // for (int i=0;i<n;i++)
+        // {
+        //     if(!vis[i])
+        //     {
+        //         bool tmp=solve(i,vis,path,adj);
+        //         if (tmp)
+        //         {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        vector<int>in(n,0);
+        queue<int>q;
         for (int i=0;i<n;i++)
         {
-            if(!vis[i])
+            for (auto it:adj[i])
             {
-                bool tmp=solve(i,vis,path,adj);
-                if (tmp)
+                in[it]++;
+            }
+        }
+        int cnt=0;
+        for (int i=0;i<n;i++)
+        {
+            if (in[i]==0)
+            {
+                cnt++;
+                q.push(i);
+            }
+        }
+        while (!q.empty())
+        {   
+            int node=q.front();
+            q.pop();
+            for (auto it:adj[node])
+            {
+                in[it]--;
+                if (in[it]==0)
                 {
-                    return true;
+                    cnt++;
+                    q.push(it);
                 }
             }
         }
-        return false;
+        return cnt!=n;
         // code here
     }
 };
